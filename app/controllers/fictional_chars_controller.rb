5bc85a538a88e7b2a25001ebb4a1bdd9bc81b513
mikechavo <mchavez76@yahoo.com>
2023-09-06 12:10:33 -0700

    valid

--- app/controllers/fictional_chars_controller.rb
+++ app/controllers/fictional_chars_controller.rb
@@ -1,25 +1,26 @@
 class FictionalCharsController < ApplicationController
+  def index
+    fictional_chars = FictionalChar.all
+    render json: fictional_chars   
+  end
 
-    def index
-     fictional_chars = FictionalChar.all
-     render json: fictional_chars   
-    end
+  def create
+    fictional_char = FictionalChar.create(fictional_char_params)
+    if fictional_char.valid?
+      render json: fictional_char
+    else
+      render json: fictional_char.errors, status: 422
+    end 
+  end
 
-    def create
-        fictional_char = FictionalChar.create(fictional_char_params)
-        render json: fictional_char
-    end
+  def update
+  end
 
-    def update
-    end
+  def destroy
+  end
 
-    def destroy
-    end
-
-end
-
-private
-    def fictional_char_params 
+  private
+  def fictional_char_params 
     params.require(:fictional_char).permit(:name, :age, :enjoys, :image)
-    end
+  end
 end
